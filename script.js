ee0fb9403ca55e4015e00b3f8e3a74a5ce15e49e
ValeriaBodrova <110822668+ValeriaBodrova@users.noreply.github.com>
2023-10-24 13:27:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,30 +13,37 @@ let currentImageIndex = 0;
 const imgElement = document.getElementById('slider-img');
 const nextButton = document.getElementById('next');
 const prevButton = document.getElementById('prev');
+let isAnimating = false;
 prevButton.style.display = 'none'; 
 
-nextButton.addEventListener('click', function() {
+function switchImage() {
     imgElement.style.opacity = '0';
     setTimeout(() => {
-        currentImageIndex++;
         if (currentImageIndex >= images.length - 1) {
             nextButton.style.display = 'none';
+        } else if (currentImageIndex <= 0) {
+            prevButton.style.display = 'none';
+        } else {
+            nextButton.style.display = '';
+            prevButton.style.display = '';
         }
-        prevButton.style.display = '';
+
         imgElement.src = images[currentImageIndex];
         imgElement.style.opacity = '1';
-    }, 500); 
+        isAnimating = false;  // завершення анімації
+    }, 500);
+}
+
+nextButton.addEventListener('click', function() {
+    if (isAnimating) return;  // якщо анімація триває, вихід
+    isAnimating = true; 
+    currentImageIndex++;
+    switchImage();
 });
 
 prevButton.addEventListener('click', function() {
-    imgElement.style.opacity = '0';
-    setTimeout(() => {
-        currentImageIndex--;
-        if (currentImageIndex <= 0) {
-            prevButton.style.display = 'none';
-        }
-        nextButton.style.display = '';
-        imgElement.src = images[currentImageIndex];
-        imgElement.style.opacity = '1';
-    }, 500); 
+    if (isAnimating) return;  // якщо анімація триває, вихід
+    isAnimating = true; 
+    currentImageIndex--;
+    switchImage();
 });
